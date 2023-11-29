4129d33b41b5e1a34ef2f9bceb037545b3354f1c
marco <marcoperrone87@gmail.com>
2023-11-29 11:09:21 +0100

    Global css settings created. FIrst button component created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ButtonDefault from './components/ButtonDefault';
 
 function App() {
   return (
-    <></>
+    <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <ButtonDefault />
+    </div>
   );
 }
 
---
+++ src/components/ButtonDefault.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../../src/global.css';
+import './ButtonDefault.css';
+
+function ButtonDefault() {
+  return (
+    <button className="btn-default">BUTTON</button>
+  );
+}
+
+export default ButtonDefault;

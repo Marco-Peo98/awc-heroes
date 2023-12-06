692c7297494caa9f428fe196a13e12a169d35356
marco <marcoperrone87@gmail.com>
2023-12-06 17:26:34 +0100

    Made changes to Menu.js and Navbar.css Now the setOpen function is working on links too

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,11 +13,11 @@ function Menu ({ open, setOpen }) {
       </div>
       <div className="app__menu-content">
         <ul className="app__menu-list">
-          <li className="txt-white ff-title fw-bold"><a href="#home">HOME</a></li>
-          <li className="txt-white ff-title fw-bold"><a href="#heroes">EROI</a></li>
-          <li className="txt-white ff-title fw-bold"><a href="#catalog">CATALOGO</a></li>
-          <li className="txt-white ff-title fw-bold"><a href="#shop">NEGOZIO</a></li>
-          <li className="txt-white ff-title fw-bold"><a href="#exchanges">SCAMBI</a></li>
+          <li className="txt-white ff-title fw-bold"><a href="#home" onClick={() => setOpen(false)} >HOME</a></li>
+          <li className="txt-white ff-title fw-bold"><a href="#heroes" onClick={() => setOpen(false)} >EROI</a></li>
+          <li className="txt-white ff-title fw-bold"><a href="#catalog" onClick={() => setOpen(false)} >CATALOGO</a></li>
+          <li className="txt-white ff-title fw-bold"><a href="#shop" onClick={() => setOpen(false)} >NEGOZIO</a></li>
+          <li className="txt-white ff-title fw-bold"><a href="#exchanges" onClick={() => setOpen(false)} >SCAMBI</a></li>
         </ul>
       </div>
     </div>
